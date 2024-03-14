0b3cc4075cb15c3830375ef76ea327a0e6cbaf0f
daykrois <saiyanqlwy@foxmail.com>
2024-03-14 22:11:34 +0800

    完成循环

--- src/download-dm5.mjs
+++ src/download-dm5.mjs
@@ -25,24 +25,34 @@ const css = `
         headless: false // 设置为 true 以在无头模式下运行
     });
     const page = await browser.newPage();
-    await page.goto('https://www.dm5.com/m5338/', { waitUntil: 'networkidle0' });
-
-    // 等待图片元素加载完成
-    await page.waitForSelector('#cp_image');
-
-    await page.addStyleTag({ content: css });
-
-    // 捕获特定元素的截图
-    const imgElement = await page.$('img#cp_image');
-    const buffer = await imgElement.screenshot();
-
-
+    await page.goto('https://tel.dm5.com/m5338/', { waitUntil: 'networkidle0' });
+    //创建目录
     checkAndCreateDirectory(downloadPath);
-
-    // 保存截图到本地文件系统
-    const filename = path.join(downloadPath, 'downloaded-image.jpg');
-    fs.writeFileSync(filename, buffer);
-
+    let pageNum = 1;
+    while (pageNum < 5) {
+        await getImg(page, css, downloadPath, pageNum);
+        pageNum += 1;
+        const element = await page.$('a[href^="javascript:ShowNext();"]');
+        //下一页
+        if (element) {
+            await element.click();
+        } else {
+            await browser.close();
+        }
+    }
     await browser.close();
 
 })();
+
+async function getImg(page, css, downloadPath, comicName) {
+    // 等待图片元素加载完成
+    await page.waitForSelector('#cp_image');
+    await page.waitForFunction('document.readyState === "complete" && document.fonts.ready');
+    await page.addStyleTag({ content: css });
+    // 捕获特定元素的截图
+    const imgElement = await page.$('img#cp_image');
+    const buffer = await imgElement.screenshot();
+    // 保存截图到本地文件系统
+    const filename = path.join(downloadPath, `${comicName}.jpg`);
+    fs.writeFileSync(filename, buffer);
+}
