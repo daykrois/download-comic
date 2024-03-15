193d126b7628a01b2dfecade2a4cfdbe029a1019
daykrois <saiyanqlwy@foxmail.com>
2024-03-15 18:27:31 +0800

    完成一章的下载

--- src/download-dm5.mjs
+++ src/download-dm5.mjs
@@ -1,8 +1,10 @@
 import puppeteer from 'puppeteer-core';
+import fs from 'fs';
+import path from 'path';
 import createDirectoryIfNotExists from './directoryUtils.mjs';
 
-const directoryPath = '../downloads/火凤燎原/'; 
-createDirectoryIfNotExists(directoryPath);
+const downloadPath = '../downloads/火凤燎原/';
+createDirectoryIfNotExists(downloadPath);
 
 
 const css = `
@@ -17,51 +19,48 @@ const css = `
 (async () => {
     const browser = await puppeteer.launch({
         executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
-        headless: false // 设置为 true 以在无头模式下运行
+        headless: true // 设置为 true 以在无头模式下运行
     });
     const page = await browser.newPage();
     let pageNum = 192;
-    while (true) {
-        await page.goto('https://tel.dm5.com/m5338-p' + pageNum, { waitUntil: 'networkidle2' });
+    const pageCount = 193;
+    await page.goto('https://www.dm5.com/m5338-p'+pageNum, { waitUntil: 'networkidle2', timeout: 0 });
+    while (pageNum <= pageCount) {
         try {
-            const element = await page.$('span.active.right-arrow');
-            if (!element) {
-                console.log('指定元素不存在，可能已经到达最后一页或页面结构发生变化。');
-                break; // 退出循环
-            }
-            const textContent = await page.evaluate('el => el.textContent.trim()', element);
-            console.log(textContent)
-            let ImgName = textContent + '-' + pageNum;
-            // await getImg(page, css, downloadPath, ImgName);
+            await page.waitForFunction(`document.querySelector('p#imgloading') && document.defaultView.getComputedStyle(document.querySelector('p#imgloading')).display === 'none'`, { timeout: 10000 });
+            await getImg(page, css, downloadPath, pageNum);
+            await pageNext(page);
             pageNum += 1;
-
         } catch (error) {
             console.log('发生错误');
-            await browser.close();
+            break;
         }
     }
     await browser.close();
 })();
 
 async function getImg(page, css, downloadPath, comicName) {
-    // 等待图片元素加载完成
-    await page.waitForSelector('#cp_image');
-    // await page.waitForFunction('document.readyState === "complete" && document.fonts.ready');
+    console.log('开始下载第' + comicName + '页')
+    await page.waitForSelector('img#cp_image');
+    console.log('图片加载完成')
     await page.addStyleTag({ content: css });
-    // 捕获特定元素的截图
-    const imgElement = await page.$('img#cp_image');
+    console.log('添加样式完成，去掉多余元素')
+    let imgElement = await page.$('img#cp_image');
+    console.log('获取图片元素')
     const buffer = await imgElement.screenshot();
-    // 保存截图到本地文件系统
+    console.log('获取截图buffer')
     const filename = path.join(downloadPath, `${comicName}.jpg`);
+    console.log('保存截图到本地文件系统')
     fs.writeFileSync(filename, buffer);
+    console.log('截图已保存到', filename);
 }
 
 //下一页
 async function pageNext(page) {
-    const element = await page.$('a[href^="javascript:ShowNext();"]');
-    if (element) {
-        await element.click();
-    } else {
-        await browser.close();
-    }
+    const elementHandle = await page.$('#cp_image');
+    await elementHandle.click({ button: 'left', });
+    console.log('下一页')
 }
+
+
+
